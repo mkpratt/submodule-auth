618a276d3e30a10297ecc78e4b1d8aabd9f11f53
mkpratt <mkpratt6@gmail.com>
2019-08-15 00:34:42 -0500

    test submodule update

--- server.js
+++ server.js
@@ -54,4 +54,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const port = process.env.PORT || 5000
+console.log('HELLO FROM SUBMODULE CHANGE :)')
 app.listen(port, () => console.log(`Server running on port ${port}`))
